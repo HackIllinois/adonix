3d39355a93ff62e50a55e98e4e0903e2ae5ca95b
Aydan Pirani <aydanpirani@gmail.com>
2023-08-13 23:58:45 -0700

    Common values for entire service

---
+++ api/models.ts
@@ -0,0 +1,10 @@
+export enum Role {
+	ADMIN = "Admin",
+	STAFF = "Staff",
+	MENTOR = "Mentor",
+	APPLICANT = "Applicant",
+	ATTENDEE = "Attendee",
+	USER = "User",
+	SPONSOR = "Sponsor",
+	BLOBSTORE = "Blobstore",
+}
