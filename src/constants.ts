5e7a4ee3aeb80aa10cc277fcde906a8102c26fee
Aydan Pirani <aydanpirani@gmail.com>
2023-08-14 22:46:39 -0700

    Hotfix: Update Google callback URL

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ abstract class Constants {
 
 	// URLs
 	static readonly GITHUB_OAUTH_CALLBACK:string = "http://localhost:3000/auth/github/callback/";
-	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/google/callback/";
+	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://localhost:3000/auth/google/callback/";
 
 	static readonly SYSTEM_ADMIN_LIST:string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
 }
