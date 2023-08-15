899f6be2218588bb5d32c0ecefafdaf3e5dfd7bb
Aydan Pirani <aydanpirani@gmail.com>
2023-08-14 22:41:35 -0700

    Hotfix: Test in localhost

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ abstract class Constants {
 	static readonly INTERNAL_ERROR:number = 500;
 
 	// URLs
-	static readonly GITHUB_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/github/callback/";
+	static readonly GITHUB_OAUTH_CALLBACK:string = "http://localhost:3000/auth/github/callback/";
 	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/google/callback/";
 
 	static readonly SYSTEM_ADMIN_LIST:string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
