2da38bbfec5346adfb6aa61d162c5ea479181c01
Aydan Pirani <aydanpirani@gmail.com>
2023-08-14 20:39:49 -0700

    Modified constants to be prod values

--- src/constants.ts
+++ src/constants.ts
@@ -13,8 +13,7 @@ abstract class Constants {
 
 	// URLs
 	static readonly GITHUB_OAUTH_CALLBACK:string = "https://hackillinois.org/auth/github/callback/";
-	// static readonly GOOGLE_OAUTH_CALLBACK:string = "https://hackillinois.org/auth/google/callback/";
-	static readonly GOOGLE_OAUTH_CALLBACK:string = "http://localhost:3000/auth/google/callback/";
+	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://hackillinois.org/auth/google/callback/";
 
 	static readonly SYSTEM_ADMIN_LIST:string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
 }
