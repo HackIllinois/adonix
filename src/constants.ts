ec94aac15df3ab7e0b83ed34afa07e4dfe263021
Aydan Pirani <aydanpirani@gmail.com>
2023-08-14 23:12:42 -0700

    Hotfix: Re-add domain to callbacks

--- src/constants.ts
+++ src/constants.ts
@@ -12,8 +12,8 @@ abstract class Constants {
 	static readonly INTERNAL_ERROR:number = 500;
 
 	// URLs
-	static readonly GITHUB_OAUTH_CALLBACK:string = "http://localhost:3000/auth/github/callback/";
-	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://localhost:3000/auth/google/callback/";
+	static readonly GITHUB_OAUTH_CALLBACK:string = "http://adonix.hackillinois.org/auth/github/callback/";
+	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/google/callback/";
 
 	static readonly SYSTEM_ADMIN_LIST:string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
 }
