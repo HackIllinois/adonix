70c4be0ed73f09764fa50320f22f517fc1e34353
Aydan Pirani <aydanpirani@gmail.com>
2023-08-17 01:31:53 -0700

    Cleaned up constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,18 +5,20 @@ abstract class Constants {
 	static readonly SUCCESS:number = 200;
 	static readonly BAD_REQUEST:number = 400;
 	static readonly FAILURE:number = 400;
-	static readonly UNAUTHORIZED_REQUEST: number = 401;
-	static readonly FORBIDDEN: number = 403;
+	static readonly UNAUTHORIZED_REQUEST:number = 401;
+	static readonly FORBIDDEN:number = 403;
 	static readonly NOT_FOUND:number = 404;
 	static readonly OLD_API:number = 418;
 	static readonly INTERNAL_ERROR:number = 500;
 
 	// URLs
 	static readonly GITHUB_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/github/callback/";
-	// static readonly GITHUB_OAUTH_CALLBACK:string = "http://localhost:3000/auth/github/callback/";
 	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/google/callback/";
 
 	static readonly SYSTEM_ADMIN_LIST:string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
+
+	// Conversions for datetimes
+	static readonly MILLISECONDS_PER_SECOND:number = 1000;
 }
 
 export default Constants;
