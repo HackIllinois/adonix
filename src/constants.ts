d4747f9b90ccb06d235a931734f90e03c13b932f
Aydan Pirani <aydanpirani@gmail.com>
2023-08-27 12:21:50 -0500

    Hotfix: Revert to original Github Auth

--- src/constants.ts
+++ src/constants.ts
@@ -12,8 +12,7 @@ abstract class Constants {
 	static readonly INTERNAL_ERROR:number = 500;
 
 	// URLs
-	// static readonly GITHUB_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/github/callback/";
-	static readonly GITHUB_OAUTH_CALLBACK:string = "https://hackillinois.org/auth?redirect_uri=https://hackillinois.org/auth/?isiOS=1";
+	static readonly GITHUB_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/github/callback/";
 	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/google/callback/";
 
 	static readonly SYSTEM_ADMIN_LIST:string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
