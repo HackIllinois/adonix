0c35f4f6fad2546ec90f64a0b65897f3e99f6218
Aydan Pirani <aydanpirani@gmail.com>
2023-09-01 01:34:42 -0500

    reverted constants to prod

--- src/constants.ts
+++ src/constants.ts
@@ -33,8 +33,8 @@ abstract class Constants {
 
 	static readonly DEVICE_LIST: string[] = [ this.IOS_DEVICE, this.ANDROID_DEVICE, this.WEB_DEVICE, this.DEFAULT_DEVICE ];
 
-	// static readonly GITHUB_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/github/callback/";
-	static readonly GITHUB_OAUTH_CALLBACK:string = "http://localhost:3000/auth/github/callback/";
+	static readonly GITHUB_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/github/callback/";
+	// static readonly GITHUB_OAUTH_CALLBACK:string = "http://localhost:3000/auth/github/callback/";
 	static readonly GOOGLE_OAUTH_CALLBACK:string = "https://adonix.hackillinois.org/auth/google/callback/";
 
 	static readonly SYSTEM_ADMIN_LIST:string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
