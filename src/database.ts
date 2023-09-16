c561800d587e5981594de44f10321bffbc4f15c6
Jareth Gomes <jarethgomes@gmail.com>
2023-09-15 20:01:08 -0500

    Added `DB_URI` env to allow for localhost databases

--- src/database.ts
+++ src/database.ts
@@ -6,11 +6,11 @@ const password: string | undefined = process.env.DB_PASSWORD;
 const server: string | undefined = process.env.DB_SERVER;
 
 const credInfo: string = (username) ? `${encodeURIComponent(username)}:${encodeURIComponent(password || "")}@` : "";
-if (!server) {
-	throw new Error("server URI not provided in .env file!");
-}
 
-const uri = `mongodb+srv://${credInfo}${server}`;
+const uri = process.env.DB_URI || (server) ? `mongodb+srv://${credInfo}${server}` : undefined;
+if (!uri) {
+	throw new Error("No URI was able to be constructed or was provided in .env file!");
+}
 
 const client = new MongoClient(uri, {
 	retryWrites: true,
