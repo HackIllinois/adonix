e8d1f04008d3ab666e3235e6efa1c7e4a7e5c49c
Aydan Pirani <aydanpirani@gmail.com>
2023-09-17 12:19:38 -0500

    More devdoc updates

--- src/services/event/event-lib.ts
+++ src/services/event/event-lib.ts
@@ -4,6 +4,7 @@ import { BaseEvent, GenericEvent, PrivateEvent } from "./event-models";
 /**
  * Convert a noncamelcased event into a more camelcased event
  * @param baseEvent Uncapitalized event to convert into camelcase
+ * @param hasElevatedPerms Whether or not the results are going to an elevated user (display private events?)
  * @returns Camelcased event
  */
 export function camelcaseEvent(baseEvent: BaseEvent, hasElevatedPerms: boolean): GenericEvent {
