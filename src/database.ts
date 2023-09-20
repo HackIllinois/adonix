85cef7667d3742d8bf2f9331394a41440fbe16b4
Aydan Pirani <aydanpirani@gmail.com>
2023-09-20 02:27:46 -0500

    Got basic event functionality up to speed with database redesign

--- src/database.ts
+++ src/database.ts
@@ -7,12 +7,13 @@ const server: string | undefined = process.env.DB_SERVER;
 
 const credInfo: string = (username) ? `${encodeURIComponent(username)}:${encodeURIComponent(password || "")}@` : "";
 
-const uri = process.env.DB_URI || (server) ? `mongodb+srv://${credInfo}${server}` : undefined;
+const uri: string | undefined = process.env.DB_URI || (server) ? `mongodb+srv://${credInfo}${server}` : undefined;
+
 if (!uri) {
 	throw new Error("No URI was able to be constructed or was provided in .env file! Please either set DB_URI or set DB_SERVER alongside DB_USERNAME and DB_PASSWORD if applicable.");
 }
 
-const client = new MongoClient(uri, {
+const client: MongoClient = new MongoClient(uri, {
 	retryWrites: true,
 	w: "majority",
 });
