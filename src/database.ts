a99937100dc390e7efc3bacf1235920107ccba3e
Aydan Pirani <aydanpirani@gmail.com>
2023-09-24 12:42:20 -0500

    cleaned up uri error

--- src/database.ts
+++ src/database.ts
@@ -5,12 +5,7 @@ const username: string | undefined = process.env.DB_USERNAME;
 const password: string | undefined = process.env.DB_PASSWORD;
 const server: string | undefined = process.env.DB_SERVER;
 
-const uri: string | undefined = `mongodb+srv://${username}:${password}@${server}/?retryWrites=true&w=majority`;
-
-if (!uri) {
-	throw new Error("No URI was able to be constructed or was provided in .env file! Please either set DB_URI or set DB_SERVER alongside DB_USERNAME and DB_PASSWORD if applicable.");
-}
-
+const uri: string = `mongodb+srv://${username}:${password}@${server}/?retryWrites=true&w=majority`;
 const client: MongoClient = new MongoClient(uri);
 
 export default client;
