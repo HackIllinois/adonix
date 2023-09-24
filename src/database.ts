36478bf12ac2156e9bf56e7a1ac2a0d870df772c
Aydan Pirani <aydanpirani@gmail.com>
2023-09-24 12:51:59 -0500

    updated uri

--- src/database.ts
+++ src/database.ts
@@ -6,6 +6,7 @@ const password: string | undefined = process.env.DB_PASSWORD;
 const server: string | undefined = process.env.DB_SERVER;
 
 const uri: string = `mongodb+srv://${username}:${password}@${server}/?retryWrites=true&w=majority`;
+console.log(uri);
 const client: MongoClient = new MongoClient(uri);
 
 export default client;
