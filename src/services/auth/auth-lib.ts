1242fae33be6b66322ab3539e6c385ba3c501952
Akul Sharma <akulsharma02@gmail.com>
2023-09-28 00:11:41 -0500

    changed jwt errors to jwt specific error type

--- src/services/auth/auth-lib.ts
+++ src/services/auth/auth-lib.ts
@@ -153,13 +153,13 @@ export function generateJwtToken(payload?: JwtPayload, expiration?: string): str
  */
 export function decodeJwtToken(token?: string): JwtPayload {
 	if (!token) {
-		throw new Error("NoToken");
+		throw new JWTError("NoToken");
 	}
 
 	// Ensure that we have a secret to parse token
 	const secret: string | undefined = process.env.JWT_SECRET;
 	if (!secret) {
-		throw new Error("NoSecret");
+		throw new JWTError("NoSecret");
 	}
 
 	// Verify already ensures that the token isn't expired. If it is, it returns an error
