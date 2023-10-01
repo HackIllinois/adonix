be505904346dcd5e794455bed323a78bd573c24c
Aydan Pirani <aydanpirani@gmail.com>
2023-10-01 12:11:20 -0500

    Fixed git issues

--- src/services/newsletter/newsletter-formats.ts
+++
@@ -1,6 +0,0 @@
-
-// Request format for subscribe API calls
-export interface SubscribeRequest {
-	listName: string,
-    emailAddress: string
-}
--- src/services/user/user-formats.ts
+++
@@ -1,7 +0,0 @@
-export interface UserFormat {
-	id: string,
-	username: string,
-	firstname: string,
-	lastname: string,
-	email: string,
-}
