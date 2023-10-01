1b22cbfad38a721b03aacbecbe12db8b0a1484ed
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-10-01 12:55:07 -0500

    Fix eslint & add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,63 +1,39 @@
-/* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    ignorePatterns: ["temp.js", "**/devdocs/**", "**/apidocs/**", "**/scripts/**"],
-    parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname
+    env: {
+        es2021: true,
+        node: true,
     },
-    root: true,
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ["@typescript-eslint"],
     rules: {
-        "@typescript-eslint/typedef": ["error",
+        "@typescript-eslint/no-misused-promises": [
+            "error",
             {
-                "arrowParameter": true,
-                "variableDeclaration": true,
-                "memberVariableDeclaration": true,
-                "parameter": true,
-                "propertyDeclaration": true
-            }
-        ],
-        "@typescript-eslint/no-misused-promises": ["error",
-            {
-                "checksVoidReturn": false
-            }
+                checksVoidReturn: false,
+            },
         ],
         "@typescript-eslint/explicit-function-return-type": "error",
         "@typescript-eslint/no-unsafe-argument": "error",
         "@typescript-eslint/no-explicit-any": "error",
-        "linebreak-style": ["error", "unix"],
         "consistent-return": ["error"],
         // Suggestions
         "arrow-body-style": ["error", "always"],
-        "curly": ["error", "all"],
+        curly: ["error", "all"],
         "no-lonely-if": ["error"],
-        "no-magic-numbers": ["error", { "ignoreClassFieldInitialValues": true }],
+        "no-magic-numbers": ["error", { ignoreClassFieldInitialValues: true }],
         "no-multi-assign": ["error"],
         "no-nested-ternary": ["error"],
         "no-var": ["error"],
         "prefer-const": ["error"],
-        "strict": ["error", "global"],
-        "yoda": ["error", "never"],
-        // Layout & Formatting
-        "array-bracket-spacing": ["error", "always", { "singleValue": false }],
-        "arrow-parens": ["error", "always"],
-        "block-spacing": ["error", "always"],
-        "brace-style": ["error", "1tbs"],
-        "comma-dangle": ["error", "always-multiline"],
-        "comma-style": ["error", "last"],
-        "eol-last": ["error", "always"],
-        "func-call-spacing": ["error", "never"],
-        "indent": ["error", "tab"],
-        "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
-        "keyword-spacing": ["error", { "before": true, "after": true }],
-        "line-comment-position": ["error", { "position": "above" }],
-        "new-parens": ["error", "always"],
-        "no-multi-spaces": ["error"],
-        "no-trailing-spaces": ["error", { "skipBlankLines": true, "ignoreComments": false }],
-        "object-curly-spacing": ["error", "always"],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-    }
-}
+        strict: ["error", "global"],
+        yoda: ["error", "never"],
+    },
+};
