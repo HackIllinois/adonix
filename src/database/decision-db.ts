e6da23b461655a272447173148904d741fb8546c
Aydan Pirani <aydanpirani@gmail.com>
2023-10-07 19:48:28 -0500

    finished database model work

--- src/database/decision-db.ts
+++ src/database/decision-db.ts
@@ -2,49 +2,49 @@ import { getModelForClass, mongoose, prop } from "@typegoose/typegoose";
 import { Databases, generateConfig } from "database.js";
 
 enum DecisionDB {
-    INFO="info", 
-    ENTRIES="entries"
+    INFO = "info",
+    ENTRIES = "entries",
 }
 
 enum DecisionStatus {
-    TBD="TBD",
-    ACCEPTED="ACCEPTED",
-    REJECTED="REJECTED",
-    WAITLISTED="WAITLISTED"
+    TBD = "TBD",
+    ACCEPTED = "ACCEPTED",
+    REJECTED = "REJECTED",
+    WAITLISTED = "WAITLISTED",
 }
 
 enum DecisionResponse {
-    PENDING="PENDING",
-    ACCEPTED="ACCEPTED",
-    DECLINED="DECLINED",
+    PENDING = "PENDING",
+    ACCEPTED = "ACCEPTED",
+    DECLINED = "DECLINED",
 }
 
 export class DecisionInfo {
-    @prop( {required: true} )
+    @prop({ required: true })
     public _id: string;
 
-    @prop( {required: true} )
+    @prop({ required: true })
     public userId: string;
 
-    @prop( {required: true} )
+    @prop({ required: true })
     public status: DecisionStatus;
 
-    @prop( {required: true} )
+    @prop({ required: true })
     public response: DecisionResponse;
 }
 
 export class DecisionEntry {
-    @prop( {required: true} )
+    @prop({ required: true })
     public _id: string;
-    @prop( {required: true} )
+    @prop({ required: true })
     public userId: string;
-    @prop( {required: true} )
+    @prop({ required: true })
     public wave: number;
-    @prop( {required: true} )
+    @prop({ required: true })
     public reviewer: string;
-    @prop( {required: true} )
+    @prop({ required: true })
     public timestamp: number;
-    @prop( {required: true} )
+    @prop({ required: true })
     public decision: DecisionStatus;
 }
 
---
+++ src/database/registration-db.ts
@@ -0,0 +1,39 @@
+import { prop, getModelForClass, mongoose } from "@typegoose/typegoose";
+import { Databases, generateConfig } from "database.js";
+
+enum RegistrationDB {
+    INFO = "info",
+    APPLICATION = "application",
+}
+
+export class RegistrationInfo {
+    @prop({ required: true })
+    public _id: string;
+
+    @prop({ required: true })
+    public userId: string;
+
+    @prop({ required: true })
+    public preferredName: string;
+
+    @prop({ required: true })
+    public userName: string;
+}
+
+export class Application {
+    @prop({ required: true })
+    public _id: string;
+
+    @prop({ required: true })
+    public userId: string;
+}
+
+export const DecisionInfoModel: mongoose.Model<RegistrationInfo> = getModelForClass(
+    RegistrationInfo,
+    generateConfig(Databases.DECISION_DB, RegistrationDB.INFO),
+);
+
+export const ApplicationModel: mongoose.Model<Application> = getModelForClass(
+    Application,
+    generateConfig(Databases.DECISION_DB, RegistrationDB.APPLICATION),
+);
