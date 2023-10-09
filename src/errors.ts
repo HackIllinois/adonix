762fb58b0635c9799af886faa1139424e01b54c2
Akul Sharma <akulsharma02@gmail.com>
2023-10-08 23:06:53 -0500

    deleted errors

--- src/errors.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// class JWTError extends Error {
-// 	constructor(message: string) {
-// 		super(message);
-// 		this.name = "JWTError";
-// 		Error.captureStackTrace(this, JWTError);
-// 	}
-// }
