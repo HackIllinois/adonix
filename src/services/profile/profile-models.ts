ebfe6a3f7b6444cda6b004f2d1b86ac96686eee5
Akul Sharma <akulsharma02@gmail.com>
2023-10-08 23:07:38 -0500

    deleted unnecessary interface

--- src/services/profile/profile-models.ts
+++ src/services/profile/profile-models.ts
@@ -3,13 +3,3 @@ export interface LeaderboardEntry {
     points: number;
     displayName: string;
 }
-
-// export interface Profile {
-//     id: string,
-//     firstName: string,
-//     lastName: string,
-//     points: number,
-//     discord: string,
-//     avatarUrl: string,
-//     foodWave: number,
-// }
