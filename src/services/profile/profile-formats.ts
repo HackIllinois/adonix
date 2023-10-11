edcb525910c80072c97563805f477b3df0ec9822
Akul Sharma <akulsharma02@gmail.com>
2023-10-11 15:59:23 -0500

    added profile format checker

---
+++ src/services/profile/profile-formats.ts
@@ -0,0 +1,11 @@
+import { AttendeeProfile } from "../../database/attendee-db.js";
+
+export function isValidProfileModel(profile: AttendeeProfile): boolean {
+    if (!profile)
+        return false;
+    if (!profile.avatarUrl || !profile.discordName || !profile.displayName) 
+        return false;
+    if (typeof profile.discordName !== "string" || typeof profile.displayName !== "string" || typeof profile.avatarUrl !== "string")
+        return false;
+    return true;
+}
