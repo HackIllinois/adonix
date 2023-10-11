a36899c2a96d2248d58cb8274d0a5eb1b2357831
Akul Sharma <akulsharma02@gmail.com>
2023-10-11 16:05:18 -0500

    more formatting adjustments (pls work)

--- src/services/profile/profile-formats.ts
+++ src/services/profile/profile-formats.ts
@@ -1,13 +1,18 @@
 import { AttendeeProfile } from "../../database/attendee-db.js";
 
 export function isValidProfileModel(profile: AttendeeProfile): boolean {
-    if (!profile) {return false;}
-    if (!profile.avatarUrl || !profile.discordName || !profile.displayName) {return false;}
+    if (!profile) {
+        return false;
+    }
+    if (!profile.avatarUrl || !profile.discordName || !profile.displayName) {
+        return false;
+    }
     if (
         typeof profile.discordName !== "string" ||
         typeof profile.displayName !== "string" ||
         typeof profile.avatarUrl !== "string"
-    )
-        {return false;}
+    ) {
+        return false;
+    }
     return true;
 }
