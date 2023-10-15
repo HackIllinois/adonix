715cda3a89bfef6e9bbe6622543006ba3104b378
Aydan Pirani <aydanpirani@gmail.com>
2023-10-14 23:43:32 -0500

    Added JWT changes

--- src/constants.ts
+++ src/constants.ts
@@ -12,30 +12,27 @@ abstract class Constants {
     static readonly INTERNAL_ERROR: number = 500;
 
     // URLs
-    private static readonly ADMIN_DEVICE: string = "admin";
+    static readonly ADMIN_DEVICE: string = "admin";
+    static readonly DEV_DEVICE: string = "dev";
+    static readonly WEB_DEVICE: string = "web";
+    static readonly IOS_DEVICE: string = "ios";
+    static readonly ANDROID_DEVICE: string = "android";
+    static readonly DEFAULT_DEVICE: string = Constants.WEB_DEVICE;
+
     private static readonly ADMIN_REDIRECT: string = "https://admin.hackillinois.org/auth/";
-
-    private static readonly DEV_DEVICE: string = "dev";
     private static readonly DEV_REDIRECT: string = "https://adonix.hackillinois.org/auth/dev/";
-
-    private static readonly WEB_DEVICE: string = "web";
     private static readonly WEB_REDIRECT: string = "https://www.hackillinois.org/auth/";
-
-    private static readonly IOS_DEVICE: string = "ios";
     private static readonly IOS_REDIRECT: string = "hackillinois://login/";
-
-    private static readonly ANDROID_DEVICE: string = "android";
     private static readonly ANDROID_REDIRECT: string = "hackillinois://login/";
-
-    static readonly DEFAULT_DEVICE: string = this.WEB_DEVICE;
     static readonly DEFAULT_REDIRECT: string = this.WEB_REDIRECT;
+
 
     static readonly REDIRECT_MAPPINGS: Map<string, string> = new Map<string, string>([
         [this.ADMIN_DEVICE, this.ADMIN_REDIRECT],
         [this.WEB_DEVICE, this.WEB_REDIRECT],
         [this.IOS_DEVICE, this.IOS_REDIRECT],
         [this.ANDROID_DEVICE, this.ANDROID_REDIRECT],
-        [this.DEFAULT_DEVICE, this.DEFAULT_REDIRECT],
+        [Constants.DEFAULT_DEVICE, this.DEFAULT_REDIRECT],
         [this.DEV_DEVICE, this.DEV_REDIRECT],
     ]);
 
@@ -46,7 +43,7 @@ abstract class Constants {
 
     static readonly SYSTEM_ADMIN_LIST: string[] = (process.env.SYSTEM_ADMINS ?? "").split(",");
 
-    static readonly DEFAULT_JWT_OFFSET: string = "48h";
+    static readonly DEFAULT_JWT_OFFSET: string = "1h";
 
     // Constants for general usage
     static readonly ZERO: number = 0;
