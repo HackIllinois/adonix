17d3ff0a1e767a5c1bc5cd7fb91cf18a890e545e
Aydan Pirani <aydanpirani@gmail.com>
2023-10-14 22:53:34 -0500

    got the barebones of the versioning to work

---
+++ src/middleware/config-reader.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Request, Response } from "express";
+import Constants from "../constants.js";
+import axios, { AxiosResponse } from "axios";
+
+interface ConfigFormat {
+    androidVersion: string;
+    iosVersion: string;
+}
+
+function isValidConfigFormat(c: ConfigFormat): boolean {
+    if (typeof c.androidVersion !== "string" || typeof c.iosVersion !== "string") {
+        return false;
+    }
+
+    return true;
+}
+
+export class ConfigReader {
+    static iosVersion: string;
+    static androidVersion: string;
+
+    async initialize(): Promise<void> {
+        const url: string = Constants.METADATA_URL;
+
+        const response: AxiosResponse = await axios.get(url);
+        const configData: ConfigFormat = response.data as ConfigFormat;
+
+        console.log(configData);
+        if (!configData || !isValidConfigFormat(configData)) {
+            return Promise.reject("InvalidConfigFormat");
+        }
+
+        ConfigReader.androidVersion = configData.androidVersion;
+        ConfigReader.iosVersion = configData.iosVersion;
+
+        return Promise.resolve();
+    }
+}
+
+const configReader: ConfigReader = new ConfigReader();
+
+export function getConfigReader(): ConfigReader {
+    return configReader;
+}
+
+export async function InitializeConfigReader(_1: Request, _: Response, next: NextFunction): Promise<void> {
+    await configReader.initialize();
+    next();
+}
