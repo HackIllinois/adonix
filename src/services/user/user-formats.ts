cf97d7ce5cea5bc0c1722ccf41264eef7365857d
Aydan Pirani <aydanpirani@gmail.com>
2023-10-15 12:46:22 -0500

    Fix user router + docs (#84)
    * Fixed user service
    
    * more docs fixeS
    
    * fixed caps error
    
    * Finished services
    
    * formatted

--- src/services/user/user-formats.ts
+++ src/services/user/user-formats.ts
@@ -1,7 +1,13 @@
 export interface UserFormat {
-    id: string;
-    username: string;
-    firstname: string;
-    lastname: string;
+    userId: string;
+    name: string;
     email: string;
+}
+
+export function isValidUserFormat(u: UserFormat): boolean {
+    if (typeof u.userId !== "string" || typeof u.name !== "string" || typeof u.email !== "string") {
+        return false;
+    }
+
+    return true;
 }
