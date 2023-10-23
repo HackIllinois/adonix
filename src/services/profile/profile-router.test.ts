f5a1d6e4838a26eab84730d1e850382c9e2c6eea
Akul Sharma <akulsharma02@gmail.com>
2023-10-22 22:33:27 -0500

    test file improvements

--- src/services/profile/profile-router.test.ts
+++ src/services/profile/profile-router.test.ts
@@ -3,10 +3,41 @@ import Models from "../../database/models.js";
 import { TESTER, delAsUser, getAsAdmin, getAsUser, postAsAttendee, postAsUser } from "../../testTools.js";
 import { ProfileFormat } from "./profile-formats.js";
 import Constants from "../../constants.js";
+import { AttendeeMetadata, AttendeeProfile } from "database/attendee-db.js";
 
 beforeEach(async () => {
     Models.initialize();
+
 });
+
+const TESTER_USER = {
+    userId: TESTER.id,
+    displayName: TESTER.name,
+    avatarUrl: TESTER.avatarUrl,
+    discordTag: TESTER.discordTag,
+    points: 0,
+} satisfies AttendeeProfile;
+
+const TESTER_METADATA = {
+    userId: TESTER.id,
+    foodWave: 0,
+} satisfies AttendeeMetadata;
+
+const TESTER_USER_2 = {
+    userId: "tester2",
+    displayName: TESTER.name + "2",
+    avatarUrl: TESTER.avatarUrl,
+    discordTag: TESTER.discordTag,
+    points: 12,
+} satisfies AttendeeProfile;
+
+const TESTER_USER_3 = {
+    userId: "tester3",
+    displayName: TESTER.name + "3",
+    avatarUrl: TESTER.avatarUrl,
+    discordTag: TESTER.discordTag,
+    points: 12,
+} satisfies AttendeeProfile;
 
 const profile: ProfileFormat = {
     userId: TESTER.id,
@@ -47,20 +78,14 @@ describe("POST /profile", () => {
 });
 
 describe("GET /profile", () => {
-    it("gets with an attendee that doesn't exist", async () => {
+    it("fails to get a profile that doesn't exist", async () => {
         const response = await getAsUser("/profile/").expect(404);
 
         expect(JSON.parse(response.text)).toHaveProperty("error", "UserNotFound");
     });
 
-    it("gets an attendee that exists", async () => {
-        await Models.AttendeeProfile.create({
-            userId: TESTER.id,
-            displayName: TESTER.name,
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 0,
-        });
+    it("gets a profile that exists", async () => {
+        await Models.AttendeeProfile.create(TESTER_USER);
 
         const response = await getAsUser("/profile/").expect(200);
         expect(JSON.parse(response.text)).toHaveProperty("displayName", TESTER.name);
@@ -68,54 +93,29 @@ describe("GET /profile", () => {
 });
 
 describe("GET /profile/id/:USERID", () => {
-    it("gets without a :USERID", async () => {
-        await Models.AttendeeProfile.create({
-            userId: TESTER.id,
-            displayName: TESTER.name,
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 0,
-        });
+    it("fails with no id provided", async () => {
 
         await getAsUser("/profile/id").expect(302);
     });
 
-    it("gets with a user", async () => {
-        await Models.AttendeeProfile.create({
-            userId: "abcdefgabcdefgaaa",
-            displayName: "tst",
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 0,
-        });
+    it("fails to get a profile as a user", async () => {
+        await Models.AttendeeProfile.create(TESTER_USER);
 
-        const response = await getAsUser("/profile/id/abcdefgabcdefgaaa").expect(403);
+        const response = await getAsUser("/profile/id/"+TESTER.id).expect(403);
 
         expect(JSON.parse(response.text)).toHaveProperty("error", "Forbidden");
     });
 
     it("gets with an admin", async () => {
-        await Models.AttendeeProfile.create({
-            userId: "abcdefgabcdefgaaa",
-            displayName: "tst",
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 0,
-        });
+        await Models.AttendeeProfile.create(TESTER_USER);
 
-        const response = await getAsAdmin("/profile/id/abcdefgabcdefgaaa").expect(200);
+        const response = await getAsAdmin("/profile/id/"+TESTER.id).expect(200);
 
-        expect(JSON.parse(response.text)).toHaveProperty("displayName", "tst");
+        expect(JSON.parse(response.text)).toHaveProperty("displayName", TESTER.name);
     });
 
     it("gets a user that doesnt exist", async () => {
-        await Models.AttendeeProfile.create({
-            userId: "abcdefgabcdefgaaa",
-            displayName: "tst",
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 0,
-        });
+        await Models.AttendeeProfile.create(TESTER_USER);
 
         const response = await getAsAdmin("/profile/id/doesnotexist").expect(404);
 
@@ -124,24 +124,15 @@ describe("GET /profile/id/:USERID", () => {
 });
 
 describe("DELETE /profile/", () => {
-    it("deletes with a attendee that doesn't exist", async () => {
+    it("fails to delete a profile that doesn't exist", async () => {
         const response = await delAsUser("/profile").expect(404);
         expect(JSON.parse(response.text)).toHaveProperty("error", "AttendeeNotFound");
     });
 
-    it("deletes with a valid user", async () => {
-        await Models.AttendeeProfile.create({
-            userId: TESTER.id,
-            displayName: TESTER.name,
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 0,
-        });
+    it("deletes a profile", async () => {
+        await Models.AttendeeProfile.create(TESTER_USER);
 
-        await Models.AttendeeMetadata.create({
-            userId: TESTER.id,
-            foodWave: 0,
-        });
+        await Models.AttendeeMetadata.create(TESTER_METADATA);
 
         const response = await delAsUser("/profile").expect(200);
         expect(JSON.parse(response.text)).toHaveProperty("success", true);
@@ -149,63 +140,31 @@ describe("DELETE /profile/", () => {
 });
 
 describe("GET /profile/leaderboard", () => {
-    it("gets with no limit but only 3 entries", async () => {
-        await Models.AttendeeProfile.create({
-            userId: TESTER.id,
-            displayName: TESTER.name,
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 25,
-        });
+    it("gets 3 entries when no limit is set", async () => {
 
-        await Models.AttendeeProfile.create({
-            userId: "tester2",
-            displayName: TESTER.name + "2",
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 12,
-        });
+        await Models.AttendeeProfile.create(TESTER_USER);
 
-        await Models.AttendeeProfile.create({
-            userId: "tester3",
-            displayName: TESTER.name + "3",
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 27,
-        });
+        await Models.AttendeeProfile.create(TESTER_USER_2);
+
+        await Models.AttendeeProfile.create(TESTER_USER_3);
 
         await getAsUser("/profile/leaderboard").expect(200);
     });
 
     it("gets with a limit of 2", async () => {
-        await Models.AttendeeProfile.create({
-            userId: TESTER.id,
-            displayName: TESTER.name,
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 25,
-        });
+        await Models.AttendeeProfile.create(TESTER_USER);
 
-        await Models.AttendeeProfile.create({
-            userId: "tester2",
-            displayName: TESTER.name + "2",
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 12,
-        });
+        await Models.AttendeeProfile.create(TESTER_USER_2);
 
-        await Models.AttendeeProfile.create({
-            userId: "tester3",
-            displayName: TESTER.name + "3",
-            avatarUrl: TESTER.avatarUrl,
-            discordTag: TESTER.discordTag,
-            points: 27,
-        });
+        await Models.AttendeeProfile.create(TESTER_USER_3);
 
-        await getAsUser("/profile/leaderboard?limit=2").expect(200);
+        const response = await getAsUser("/profile/leaderboard?limit=2").expect(200);
+        
+        const responseArray = JSON.parse(response.text);
+        expect(responseArray.profiles.length).toBeLessThan(3);
     });
 
-    it("gets with no limit & more entries in db than the max query limit", async () => {
+    it("only gets the max limit when no limit is set", async () => {
         for (let i = 0; i < Constants.LEADERBOARD_QUERY_LIMIT + 15; i++) {
             await Models.AttendeeProfile.create({
                 userId: TESTER.id + " " + i,
@@ -223,7 +182,7 @@ describe("GET /profile/leaderboard", () => {
         expect(responseArray.profiles.length).toBeLessThan(Constants.LEADERBOARD_QUERY_LIMIT + 1);
     });
 
-    it("gets with an invalid limit", async () => {
+    it("fails when an invalid limit is set", async () => {
         const response = await getAsUser("/profile/leaderboard?limit=0").expect(400);
 
         expect(JSON.parse(response.text)).toHaveProperty("error", "InvalidLimit");
