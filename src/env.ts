1943379348f8795693bb9c37470b353bb25a1a44
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2023-11-03 17:03:40 -0500

    Add explanatory comment to env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,11 @@
+/*
+ * This file loads the env variables we will use at runtime. Instead of relying on system envs dynamically,
+ * we instead parse the .env file, and overwrite any existing variables with system variables.
+ * Basically, .env file vars can be overwritten by system level env vars.
+ *
+ * The .env is also optional so that env vars can be entirely defined with system vars if needed, like for vercel.
+ */
+
 import dotenv from "dotenv";
 import { existsSync, readFileSync } from "fs";
 import path from "path";
