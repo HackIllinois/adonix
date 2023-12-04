53f86c340b0320e4a11f734392849cc594137b31
Akul Sharma <akulsharma02@gmail.com>
2023-12-03 22:00:55 -0600

    set up staff model

---
+++ src/database/staff-db.ts
@@ -0,0 +1,14 @@
+import { prop } from "@typegoose/typegoose";
+
+export class StaffShift {
+    @prop({required: true})
+    public _id: string;
+
+    @prop({
+        required: true,
+        type: () => {
+            return String;
+        },
+    })
+    public shifts: string[];
+}
