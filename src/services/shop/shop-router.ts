ccabad471c21bc46864a9a92b6087eb9e48c6183
Lasya <neti.lasya@gmail.com>
2024-01-10 00:48:08 -0800

    teehee

--- src/services/shop/shop-router.ts
+++ src/services/shop/shop-router.ts
@@ -1,46 +1,14 @@
 import { Request, Response, Router } from "express";
 import { NextFunction } from "express-serve-static-core";
-import { weakJwtVerification } from "../../middleware/verify-jwt.js";
+import { weakJwtVerification, strongJwtVerification } from "../../middleware/verify-jwt.js";
+import { DeleteResult } from "mongodb";
 import { StatusCode } from "status-code-enum";
-// import { RouterError } from "../../middleware/error-handler.js";
-// import { ItemFormat, QuantityFormat, ShopItemFormat } from "./shop-formats.js";
-// import Models from "../../database/models.js";
-
-const shopRouter: Router = Router();
-// shopRouter.get("/", weakJwtVerification, async (_: Request, res: Response, next: NextFunction) => {
-//     try {
-//         const shopItems: ShopItemFormat[] = await Models.ShopItem.find();
-//         const shopQuantities: QuantityFormat[] = await Models.ShopQuantity.find();
-
-//         const itemsMap = new Map<string, ShopItemFormat>();
-//         shopItems.forEach((item) => {
-//             return itemsMap.set(item.itemId, item);
-//         });
-
-//         const itemsWithQuantity: ItemFormat[] = Array.from(itemsMap.values()).map((item) => {
-//             const quantity = shopQuantities.find((q) => {
-//                 return q.itemId === item.itemId;
-//             });
-//             const itemQuantity = quantity ? quantity.quantity : 0;
-
-//             return {
-//                 itemId: item.itemId,
-//                 name: item.name || "",
-//                 price: item.price || 0,
-//                 isRaffle: item.isRaffle || false,
-//                 quantity: itemQuantity,
-//             };
-//         });
-
-//         return res.status(StatusCode.SuccessOK).send(itemsWithQuantity);
-//     } catch (error) {
-//         console.error(error);
-//         return next(new RouterError(StatusCode.ServerErrorInternal, "InternalServerError"));
-//     }
-// });
+import { RouterError } from "../../middleware/error-handler.js";
+import { ItemFormat, QuantityFormat, ShopItemFormat } from "./shop-formats.js";
+import Models from "../../database/models.js";
 
 /**
- * @api {get} /shop/ GET /shop/
+ * @api {get} /shop GET /shop
  * @apiGroup Shop
  * @apiDescription Get item details for all items.
  *
@@ -49,38 +17,89 @@ const shopRouter: Router = Router();
  * HTTP/1.1 200 OK
  * {
  *   [{
- *      "name": "hoodie",
+ *      "itemId": "item01",
+ *      "name": "HackIllinois Branded Hoodie",
  *      "price": 15,
  *      "isRaffle": true,
- *      "quantity": 20
+ *      "quantity": 1
  *   }, {
- *      "name": "shoe",
+ *      "itemId": "item02",
+ *      "name": "Snack Pack!",
  *      "price": 20,
  *      "isRaffle": false,
- *      "quantity": 1
+ *      "quantity": 25
  *   }]
  * }
  *
  * @apiUse weakVerifyErrors
  * */
-shopRouter.get("/", weakJwtVerification, async (_1: Request, res: Response, _2: NextFunction) => {
-    const data = [
-        {
-            itemId: "0x1218907123897312891",
-            name: "sticker",
-            price: 15,
-            isRaffle: true,
-            quantity: 20,
-        },
-        {
-            itemId: "0x1218907123897312892",
-            name: "shoes",
-            price: 20,
-            isRaffle: false,
-            quantity: 1,
-        },
-    ];
-    return res.status(StatusCode.SuccessOK).send(data);
+
+const shopRouter: Router = Router();
+shopRouter.get("/", weakJwtVerification, async (_: Request, res: Response, next: NextFunction) => {
+    console.log("nesdkjfnjksfksdf");
+    try {
+        const shopItems: ShopItemFormat[] = await Models.ShopItem.find();
+        const shopQuantities: QuantityFormat[] = await Models.ShopQuantity.find();
+
+        const itemsMap = new Map<string, ShopItemFormat>();
+        shopItems.forEach((item) => {
+            return itemsMap.set(item.itemId, item);
+        });
+
+        const itemsWithQuantity: ItemFormat[] = Array.from(itemsMap.values()).map((item) => {
+            const quantity = shopQuantities.find((q) => {
+                return q.itemId === item.itemId;
+            });
+            const itemQuantity = quantity ? quantity.quantity : 0;
+
+            return {
+                itemId: item.itemId,
+                name: item.name || "",
+                price: item.price || 0,
+                isRaffle: item.isRaffle || false,
+                quantity: itemQuantity,
+            };
+        });
+
+        return res.status(StatusCode.SuccessOK).send(itemsWithQuantity);
+    } catch (error) {
+        console.error(error);
+        return next(new RouterError(StatusCode.ServerErrorInternal, "InternalServerError"));
+    }
 });
+
+/**
+ * @api {delete} /shop/item/:ITEMID DELETE /shop/item/:ITEMID
+ * @apiGroup Shop
+ * @apiDescription Delete the a specific item in the point shop based itemId.
+ *
+ * @apiSuccess (200: Success) {Json} success Indicates successful deletion of the user's profile.
+ * @apiSuccessExample Example Success Response:
+ * HTTP/1.1 200 OK
+ * {
+ *    "success": true
+ * }
+ * 
+ * @apiError (500: Internal Error) {String} InternalError An internal server error occurred.
+ * @apiErrorExample Example Error Response (InternalError):
+ *     HTTP/1.1 500 Internal Server Error
+ *     {"error": "InternalError"}
+ */
+
+shopRouter.delete("/item/:ITEMID", strongJwtVerification, async (req: Request, res: Response, next: NextFunction) => {
+    console.log("89");
+    const targetItem: string | undefined = req.params.ITEMID as string;
+    console.log("91");
+
+    const shopItemDeleteResponse: DeleteResult = await Models.ShopItem.deleteOne({ itemId: targetItem });
+    const shopQuantityDeleteResponse: DeleteResult = await Models.ShopQuantity.deleteOne({ itemId: targetItem });
+
+    if (shopItemDeleteResponse.deletedCount == 0 || shopQuantityDeleteResponse.deletedCount == 0) {
+        return next(new RouterError(StatusCode.ClientErrorNotFound, "ItemNotFound"));
+    }
+    return res.status(StatusCode.SuccessOK).send({ success: true });
+});
+
+
 
 export default shopRouter;
