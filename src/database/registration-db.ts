2712a9a8ca44b2c8a465dcb68c0940dd7a7e107c
Aydan Pirani <aydanpirani@gmail.com>
2024-01-11 02:34:01 -0800

    API support for registration  (#143)
    * Finished registration API support

--- src/database/registration-db.ts
+++ src/database/registration-db.ts
@@ -1,23 +1,80 @@
 import { prop } from "@typegoose/typegoose";
 
-export class RegistrationInfo {
-    @prop({ required: true })
-    public userId: string;
-
-    @prop({ required: true })
-    public preferredName: string;
-
-    @prop({ required: true })
-    public userName: string;
-}
-
 export class RegistrationApplication {
     @prop({ required: true })
     public userId: string;
 
     @prop({ required: true })
-    public resume: string;
+    public isProApplicant: boolean;
 
-    @prop({ required: true, type: String })
-    public essays: string[];
+    @prop({ required: true })
+    public preferredName: string;
+
+    @prop({ required: true })
+    public legalName: string;
+
+    @prop({ required: true })
+    public emailAddress: string;
+
+    @prop({ required: true })
+    public gender: string;
+
+    @prop({
+        required: true,
+        type: () => {
+            return String;
+        },
+    })
+    public race: string[];
+
+    @prop({ required: true })
+    public requestedTravelReimbursement: boolean;
+
+    @prop({ required: true })
+    public location: string;
+
+    @prop({ required: true })
+    public university: string;
+
+    @prop({ required: true })
+    public gradYear: number;
+
+    @prop({
+        required: true,
+        type: () => {
+            return String;
+        },
+    })
+    public hackInterest: string[];
+
+    @prop({
+        required: true,
+        type: () => {
+            return String;
+        },
+    })
+    public hackOutreach: string[];
+
+    @prop({
+        required: true,
+        type: () => {
+            return String;
+        },
+    })
+    public dietaryRestrictions: string[];
+
+    @prop({ required: true })
+    public hackEssay1: string;
+
+    @prop({ required: true })
+    public hackEssay2: string;
+
+    @prop({ required: true })
+    public optionalEssay?: string;
+
+    @prop({ required: true })
+    proEssay?: string;
+
+    @prop({ required: true })
+    considerForGeneral?: boolean;
 }
