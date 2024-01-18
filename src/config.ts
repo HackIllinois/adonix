0cf65eb78912a6838e7b2bb37629fd433eb4b7a1
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2024-01-17 23:07:56 -0600

    Migrate databases

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,9 @@ const Config = {
     METADATA_URL: "https://hackillinois.github.io/adonix-metadata/config.json",
 
     /* OAuth, Keys, & Permissions */
-    DB_URL: `mongodb+srv://${requireEnv("DB_USERNAME")}:${requireEnv("DB_PASSWORD")}@${requireEnv("DB_SERVER")}/`,
+    DB_URL: `mongodb+srv://${requireEnv("DB_USERNAME")}:${requireEnv("DB_PASSWORD")}@${requireEnv(
+        "DB_SERVER",
+    )}/main?retryWrites=true&w=majority"`,
     SPARKPOST_KEY: requireEnv("SPARKPOST_KEY"),
     SPARKPOST_URL: "https://api.sparkpost.com/api/v1/transmissions?num_rcpt_errors=3",
 
