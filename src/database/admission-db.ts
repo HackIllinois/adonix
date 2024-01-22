49c0abd212f18a47862993da9a8f160542ace029
Aydan Pirani <aydanpirani@gmail.com>
2024-01-21 19:25:07 -0600

    Fixed required path

--- src/database/admission-db.ts
+++ src/database/admission-db.ts
@@ -20,7 +20,7 @@ export class AdmissionDecision {
     @prop({ required: true })
     public status: DecisionStatus;
 
-    @prop({ required: true })
+    @prop({ required: false })
     public admittedPro?: boolean;
 
     @prop({ required: true })
