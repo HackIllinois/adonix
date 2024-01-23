5ce61cd751c8e9f5078e90eefb5048e007544dc6
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2024-01-22 20:37:17 -0600

    Clean up current tests

--- src/services/registration/registration-router.test.ts
+++ src/services/registration/registration-router.test.ts
@@ -1,16 +1,17 @@
 import { beforeEach, describe, expect, it } from "@jest/globals";
 import { StatusCode } from "status-code-enum";
 import Models from "../../database/models.js";
+import { RegistrationApplication } from "../../database/registration-db.js";
 import { TESTER, getAsUser, getAsAdmin, postAsUser } from "../../testTools.js";
 import { RegistrationFormat } from "./registration-formats.js";
 import { Degree, Gender, HackInterest, HackOutreach, Race } from "./registration-models.js";
 
-const GENERAL_APPLICATION = {
+const APPLICATION = {
     isProApplicant: false,
     userId: TESTER.id,
-    preferredName: "ap",
-    legalName: "ap4",
-    emailAddress: "apirani2@illinois.edu",
+    preferredName: TESTER.name,
+    legalName: TESTER.name,
+    emailAddress: TESTER.email,
     university: "ap",
     hackEssay1: "ap",
     hackEssay2: "ap",
@@ -27,64 +28,91 @@ const GENERAL_APPLICATION = {
     hackOutreach: [HackOutreach.OTHER],
 } satisfies RegistrationFormat;
 
-const UNSUBMITTED_GENERAL_REGISTRATION_DATA = { hasSubmitted: false, ...GENERAL_APPLICATION };
-const SUBMITTED_GENERAL_REGISTRATION_DATA = { hasSubmitted: true, ...GENERAL_APPLICATION };
+const UNSUBMITTED_REGISTRATION = { hasSubmitted: false, ...APPLICATION } satisfies RegistrationApplication;
+const UNSUBMITTED_OTHER_REGISTRATION = {
+    ...UNSUBMITTED_REGISTRATION,
+    userId: "otherUser",
+} satisfies RegistrationApplication;
+const SUBMITTED_REGISTRATION = { hasSubmitted: true, ...APPLICATION } satisfies RegistrationApplication;
 
-describe("GET /registration/ Endpoint", () => {
+describe("GET /registration/", () => {
     beforeEach(async () => {
-        await Models.RegistrationApplication.create(UNSUBMITTED_GENERAL_REGISTRATION_DATA);
+        await Models.RegistrationApplication.create(UNSUBMITTED_REGISTRATION);
     });
 
-    it("should retrieve user's registration data when it exists", async () => {
+    it("should retrieve registration", async () => {
         const response = await getAsUser("/registration/").expect(StatusCode.SuccessOK);
-        expect(JSON.parse(response.text)).toMatchObject(UNSUBMITTED_GENERAL_REGISTRATION_DATA);
+        expect(JSON.parse(response.text)).toMatchObject(UNSUBMITTED_REGISTRATION);
     });
 
-    it("should return 404 error when user's registration data does not exist", async () => {
-        await Models.RegistrationApplication.deleteOne({ userId: TESTER.id });
+    it("should provide a not found error when registration does not exist", async () => {
+        await Models.RegistrationApplication.deleteOne(UNSUBMITTED_REGISTRATION);
+
         const response = await getAsUser("/registration/").expect(StatusCode.ClientErrorNotFound);
         expect(JSON.parse(response.text)).toHaveProperty("error", "NotFound");
     });
 });
 
-describe("GET /registration/userid/:USERID Endpoint", () => {
+describe("GET /registration/userid/:USERID", () => {
+    const OTHER_USER_ID = UNSUBMITTED_OTHER_REGISTRATION.userId;
     beforeEach(async () => {
-        await Models.RegistrationApplication.create(UNSUBMITTED_GENERAL_REGISTRATION_DATA);
+        await Models.RegistrationApplication.create(UNSUBMITTED_OTHER_REGISTRATION);
     });
 
-    it("should retrieve user's registration data with elevated permissions", async () => {
-        const response = await getAsAdmin(`/registration/userid/${TESTER.id}`).expect(StatusCode.SuccessOK);
-        expect(JSON.parse(response.text)).toMatchObject(UNSUBMITTED_GENERAL_REGISTRATION_DATA);
+    it("should retrieve registration", async () => {
+        const response = await getAsAdmin(`/registration/userid/${OTHER_USER_ID}`).expect(StatusCode.SuccessOK);
+        expect(JSON.parse(response.text)).toMatchObject(UNSUBMITTED_OTHER_REGISTRATION);
     });
 
-    it("should return 403 error when user does not have elevated permissions", async () => {
-        const response = await getAsUser(`/registration/userid/${TESTER.id}`).expect(StatusCode.ClientErrorForbidden);
+    it("should provide a forbidden error to non-staff user", async () => {
+        const response = await getAsUser(`/registration/userid/${OTHER_USER_ID}`).expect(StatusCode.ClientErrorForbidden);
         expect(JSON.parse(response.text)).toHaveProperty("error", "Forbidden");
     });
 
-    it("should return 404 error when specified user ID does not have registration data", async () => {
-        const response = await getAsAdmin("/registration/userid/nonexistentuser").expect(StatusCode.ClientErrorNotFound);
-        expect(JSON.parse(response.text)).toHaveProperty("error", "UserNotFound");
+    it("should provide a not found error when registration does not exist", async () => {
+        await Models.RegistrationApplication.deleteOne(UNSUBMITTED_OTHER_REGISTRATION);
+
+        const response = await getAsAdmin(`/registration/userid/${OTHER_USER_ID}`).expect(StatusCode.ClientErrorNotFound);
+        expect(JSON.parse(response.text)).toHaveProperty("error", "NotFound");
     });
 });
 
-describe("POST /registration/ Endpoint", () => {
-    it("should create or update user's registration data with valid data", async () => {
-        const response = await postAsUser("/registration/").send(GENERAL_APPLICATION).expect(StatusCode.SuccessOK);
-        expect(JSON.parse(response.text)).toMatchObject(UNSUBMITTED_GENERAL_REGISTRATION_DATA);
+describe("POST /registration/", () => {
+    it("should create registration", async () => {
+        const response = await postAsUser("/registration/").send(APPLICATION).expect(StatusCode.SuccessOK);
+        expect(JSON.parse(response.text)).toMatchObject(UNSUBMITTED_REGISTRATION);
+
+        // Stored in DB
+        const stored: RegistrationApplication | null = await Models.RegistrationApplication.findOne({
+            userId: UNSUBMITTED_REGISTRATION.userId,
+        });
+        expect(stored).toMatchObject(UNSUBMITTED_REGISTRATION);
     });
 
-    it("should return 400 error when registration data is invalid", async () => {
+    it("should update registration", async () => {
+        await Models.RegistrationApplication.create({
+            ...UNSUBMITTED_REGISTRATION,
+            degree: "PhD of Data Corruption",
+        });
+        const response = await postAsUser("/registration/").send(APPLICATION).expect(StatusCode.SuccessOK);
+        expect(JSON.parse(response.text)).toMatchObject(UNSUBMITTED_REGISTRATION);
+
+        // Stored in DB
+        const stored: RegistrationApplication | null = await Models.RegistrationApplication.findOne({
+            userId: UNSUBMITTED_REGISTRATION.userId,
+        });
+        expect(stored).toMatchObject(UNSUBMITTED_REGISTRATION);
+    });
+
+    it("should provide bad request error when registration is invalid", async () => {
         const response = await postAsUser("/registration/").send({}).expect(StatusCode.ClientErrorBadRequest);
         expect(JSON.parse(response.text)).toHaveProperty("error", "BadRequest");
     });
 
-    it("should return 422 error when user has already submitted registration data", async () => {
-        await Models.RegistrationApplication.create(SUBMITTED_GENERAL_REGISTRATION_DATA);
+    it("should provide already submitted error when user has already submitted registration", async () => {
+        await Models.RegistrationApplication.create(SUBMITTED_REGISTRATION);
 
-        const response = await postAsUser("/registration/")
-            .send(GENERAL_APPLICATION)
-            .expect(StatusCode.ClientErrorUnprocessableEntity);
+        const response = await postAsUser("/registration/").send(APPLICATION).expect(StatusCode.ClientErrorUnprocessableEntity);
         expect(JSON.parse(response.text)).toHaveProperty("error", "AlreadySubmitted");
     });
 });
