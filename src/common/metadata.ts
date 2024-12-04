cf82c4ddad1fbfb86a47fb5bbad41883c66e5e49
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2024-12-04 16:59:03 -0600

    Add cache timeout to metadata

--- src/common/metadata.ts
+++ src/common/metadata.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 interface MetadataFormat {
     androidVersion: string;
     iosVersion: string;
+    retrieved: number;
 }
 
 export default class Metadata {
@@ -11,7 +12,10 @@ export default class Metadata {
 
     static async load<T extends keyof MetadataFormat>(key: T): Promise<MetadataFormat[T]> {
         if (this.metadata) {
-            return this.metadata[key];
+            const expiresAt = this.metadata.retrieved + Config.METADATA_CACHE_EXPIRY_SECONDS * Config.MILLISECONDS_PER_SECOND;
+            if (Date.now() < expiresAt) {
+                return this.metadata[key];
+            }
         }
 
         const response = await axios.get<MetadataFormat>(Config.METADATA_URL);
@@ -21,7 +25,10 @@ export default class Metadata {
             return Promise.reject("InvalidConfigFormat");
         }
 
-        this.metadata = loaded;
+        this.metadata = {
+            ...loaded,
+            retrieved: Date.now(),
+        };
 
         return this.metadata[key];
     }
