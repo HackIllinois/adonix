e25c2ac38ee1fa1abe6274e690aebc734ea6c5e5
akulsharma1 <akulsharma02@gmail.com>
2025-01-27 16:45:21 -0600

    adjusted database middleware to work with duels ws

--- src/middleware/database.ts
+++ src/middleware/database.ts
@@ -1,10 +1,34 @@
-import Config from "../common/config";
+// import Config from "../common/config";
+// import { Request, Response, NextFunction } from "express";
+// import mongoose from "mongoose";
+
+// let initialized = false;
+
+// export default async function (_req: Request, _res: Response, next: NextFunction): Promise<void> {
+//     if (!initialized || Config.TEST) {
+//         initialized = true;
+//         const uri = `${Config.DB_URL}${Config.DB_PARAMS}`;
+//         await mongoose.connect(uri).catch((e) => {
+//             initialized = false;
+//             throw e;
+//         });
+//     }
+//     next();
+// }
+
+
+// Updated database middleware (database.ts)
 import { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
+import Config from "../common/config";
 
 let initialized = false;
 
-export default async function (_req: Request, _res: Response, next: NextFunction): Promise<void> {
+export default async function (
+    req: Request | { conn?: mongoose.Connection },
+    _res: Response,
+    next: NextFunction
+): Promise<void> {
     if (!initialized || Config.TEST) {
         initialized = true;
         const uri = `${Config.DB_URL}${Config.DB_PARAMS}`;
@@ -13,5 +37,11 @@ export default async function (_req: Request, _res: Response, next: NextFunction
             throw e;
         });
     }
+    
+    // For socket.io connections, store connection on the request-like object
+    if (!(req as any).conn) {
+        (req as any).conn = mongoose.connection;
+    }
+    
     next();
 }
