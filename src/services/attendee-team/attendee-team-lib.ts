c4c2625424ff208026bf38b91b2f50a9c3e2c2d2
Sherry Long <sherryl4@illinois.edu>
2026-01-27 00:12:03 -0600

    Fix linter error

--- src/services/attendee-team/attendee-team-lib.ts
+++ src/services/attendee-team/attendee-team-lib.ts
@@ -1,5 +1,6 @@
 import Models from "../../common/models";
+import { AttendeeTeam } from "./attendee-team-schemas";
 
-export async function updateTeamPoints(team: string, amount: number) {
+export async function updateTeamPoints(team: string, amount: number): Promise<AttendeeTeam | null> {
     return Models.AttendeeTeam.findOneAndUpdate({ name: team }, { $inc: { points: amount } }, { new: true });
 }
