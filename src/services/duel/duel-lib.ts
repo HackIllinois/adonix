141344e1b3020ce55fabbd8f061b1a14e5fe77b7
Sherry Long <sherryl4@illinois.edu>
2026-02-04 20:49:49 -0600

    Fix linter error

--- src/services/duel/duel-lib.ts
+++ src/services/duel/duel-lib.ts
@@ -2,11 +2,14 @@ import Models from "../../common/models";
 import { Duel } from "./duel-schemas";
 import { updatePoints } from "../profile/profile-lib";
 
+const WINNING_SCORE = 3;
+const WINNING_POINTS = 10;
+
 export async function checkGameStatus(duelId: string, duel: Duel): Promise<void> {
     // First to reach 3 points gets 10 points
-    if (duel.hostScore == 3 || duel.guestScore == 3) {
-        const winnerId = duel.hostScore == 3 ? duel.hostId : duel.guestId;
-        await updatePoints(winnerId, 10);
+    if (duel.hostScore == WINNING_SCORE || duel.guestScore == WINNING_SCORE) {
+        const winnerId = duel.hostScore == WINNING_SCORE ? duel.hostId : duel.guestId;
+        await updatePoints(winnerId, WINNING_POINTS);
         await Models.Duel.updateOne({ _id: duelId }, { $set: { hasFinished: true } });
     }
 }
